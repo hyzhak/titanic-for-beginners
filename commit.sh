6e52a9a4616e3a86b2db66f30777c8ddcf16ad47
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2017-11-07 12:56:38 +0100

    import sklearn

---
+++ commit.sh
@@ -0,0 +1,13 @@
+set -e
+
+echo $1
+
+if [ -z "${1}" ]; then
+  echo "please define commit title: ./commit.sh 'title'"; 
+  exit 1;
+fi
+
+cp -v /Users/hyzhak/Downloads/notebook.ipynb ./notebook.ipynb
+git add .
+git commit -am "${1}"
+git push
